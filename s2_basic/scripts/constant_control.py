b493be386df844b4ba4df57ad61098c55f7db5cf
hinson <yhc3141@gmail.com>
2025-10-03 01:58:17 -0700

    feat: kill

--- s2_basic/scripts/constant_control.py
+++ s2_basic/scripts/constant_control.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import rclpy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Vector3
+from std_msgs.msg import Bool
 from rclpy.node import Node
 
 
@@ -9,14 +10,26 @@ class ConstantControl(Node):
     def __init__(self):
         super().__init__("constant_control")
         self.publisher = self.create_publisher(Twist, "/cmd_vel", 10)
+        self.subscriber = self.create_subscription(Bool, "/kill", self.kill_cb, 10)
         run_every = 0.2
         self.timer = self.create_timer(run_every, self.ping)
 
-    def ping(self):
+    def ping_twist(self, linear=None, angular=None):
         msg = Twist()
-        msg.linear.x = 1.0
-        msg.angular.z = 1.0
+        if linear: msg.linear = linear
+        if angular: msg.angular = angular
         self.publisher.publish(msg)
+
+    def ping(self):
+        self.ping_twist(
+            Vector3(x=1.0), 
+            Vector3(z=1.0),
+        )
+
+    def kill_cb(self, msg: Bool):
+        if msg.data:
+            self.timer.cancel()
+            self.ping_twist()
 
 
 def main():
